ed0f3be792f0598785acf81e42ac5f31740121ed
Shinku24 <juan.felipe.gil@correounivalle.edu.co>
2020-02-10 14:39:44 -0500

    Changed buisqueda-simbolos test interface

--- src/app/pruebas/wais/busqueda-simbolos/busqueda-simbolos.component.ts
+++ src/app/pruebas/wais/busqueda-simbolos/busqueda-simbolos.component.ts
@@ -37,7 +37,31 @@ export class BusquedaSimbolosComponent implements OnInit {
   ngOnInit() {
   }
 
+  validarRespuestas(){
+    if(this.pag1C==null){ this.pag1C = 0 }
+    if(this.pag1I==null){ this.pag1I = 0 }
+
+    if(this.pag2C==null){ this.pag2C = 0 }
+    if(this.pag2I==null){ this.pag2I = 0 }
+
+    if(this.pag3C==null){ this.pag3C = 0 }
+    if(this.pag3I==null){ this.pag3I = 0 }
+
+    if(this.pag4C==null){ this.pag4C = 0 }
+    if(this.pag4I==null){ this.pag4I = 0 }
+
+    if(this.pag5C==null){ this.pag5C = 0 }
+    if(this.pag5I==null){ this.pag5I = 0 }
+
+    if(this.pag6C==null){ this.pag6C = 0 }
+    if(this.pag6I==null){ this.pag6I = 0 }
+
+    if(this.pag7C==null){ this.pag7C = 0 }
+    if(this.pag7I==null){ this.pag7I = 0 }
+  }
+
   calcularResultado(){
+    this.validarRespuestas()
     var correctas = this.pag1C + this.pag2C + this.pag3C + this.pag4C + this.pag5C + this.pag6C + this.pag7C
     var inCorrectas = this.pag1I + this.pag2I + this.pag3I + this.pag4I + this.pag5I + this.pag6I + this.pag7I
     
@@ -48,6 +72,18 @@ export class BusquedaSimbolosComponent implements OnInit {
     }
 
     this.estado = "terminado"
+  }
+
+  aRevisar(){
+    this.estado = 'revision';
+  }
+
+  regresarAct(){
+    this.estado = 'terminado';
+  }
+
+  getResultado():number {
+    return this.resultado;
   }
 
 }
