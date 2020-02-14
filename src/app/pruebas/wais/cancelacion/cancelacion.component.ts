4ed60625ea3267890c4f426c4cb8bae8c659eead
daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2020-02-14 18:47:23 -0500

    cancelacion media interfaz

--- src/app/pruebas/wais/cancelacion/cancelacion.component.ts
+++ src/app/pruebas/wais/cancelacion/cancelacion.component.ts
@@ -11,9 +11,13 @@ export class CancelacionComponent implements OnInit {
 
   estado:String = 'instruccion';// Esta variable me dice en que estado 
 
+  test1:number[] =  [0,0]
+
+  test2:number[] =  [0,0]
+
   puntuacion:number=0;
 
-  activo:boolean=false;
+  activo:string='iniciar';
 
   pruebas:number=0;
      
@@ -27,24 +31,31 @@ export class CancelacionComponent implements OnInit {
   }
 
   iniCron(){
-    this.activo=true
+    this.activo='detener'
+    this.cronometro=44
     this.startTimer(1000);
   }
 
-    //Timer: En caso de que la imagen pase por que se acabo el tiempo se dará una calificación de 0 al item
-    startTimer(time:number) {
-      clearInterval(this.interval);
-      this.interval = setInterval(() => {
-        if(this.cronometro!==0) this.cronometro--;
-        else this.time=true
-      },time)
-    }
-
-  finPrueba(){
+  //Timer: En caso de que la imagen pase por que se acabo el tiempo se dará una calificación de 0 al item
+  startTimer(time:number) {
     clearInterval(this.interval);
-    this.cronometro=45;
-    this.estado='resul';
-    this.activo=false;
+    this.interval = setInterval(() => {
+      if(this.cronometro!==0) this.cronometro--;
+      else this.time=true
+    },time)
+  }
+
+  stopCron(){
+    clearInterval(this.interval);
+    this.activo='reiniciar';
+    this.time=false;
+  }
+
+
+  restart(){
+    clearInterval(this.interval);
+    this.activo='iniciar';
+    this.cronometro=45
     this.time=false;
   }
 
