a8d5aca30415670e9fca2f0f7c768f809cd5210c
Shinku24 <juan.felipe.gil@correounivalle.edu.co>
2020-02-13 23:32:26 -0500

    Changed pluzles-visuales test interface

--- src/app/pruebas/wais/puzles-visuales/puzles-visuales.component.ts
+++ src/app/pruebas/wais/puzles-visuales/puzles-visuales.component.ts
@@ -24,7 +24,7 @@ export class PuzlesVisualesComponent implements OnInit {
     
   }
 
-  estado:String = 'seleccion';// Esta variable me dice en que estado  
+  estado:String = 'instrucciones';// Esta variable me dice en que estado  
 
   estimulos:String[] = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg","06.jpg","07.jpg","08.jpg","09.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg",
                         "15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg","21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg","28.jpg"];
@@ -88,6 +88,7 @@ export class PuzlesVisualesComponent implements OnInit {
   
         if(!this.retorno){ //En caso de no haber fallado los items 5 0 6 sigue aumentado a partir de ahí
           this.indexActual++;
+          this.estimuloActual = "assets/estimulos/PuzlesVisuales/" + this.estimulos[this.indexActual];
         }else{ //En caso de que halla fallado el item 5 o el 6 vuelve al item 4 y empieza a disminuir desde ahí
           
           if(this.countRe===5){
@@ -168,6 +169,10 @@ export class PuzlesVisualesComponent implements OnInit {
 
   regresarAct(){
     this.estado = 'terminado';
+  }
+
+  changeState(){
+    this.estado = 'seleccion'
   }
 
   actualizarResultados(){
